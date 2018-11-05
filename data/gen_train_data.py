788e0b64192dd4d60d12c4099f2cdc84265c05f3
xuhuihero <xuhuihero@didichuxing.com>
2018-11-05 20:59:32 +0800

    train_data

---
+++ data/gen_train_data.py
@@ -0,0 +1,14 @@
+
+
+
+import sys
+import random
+
+for i in range(100000):
+    caseid = random.randint(100000, 999999)
+    label = random.randint(10, 1000)
+    id_list = []
+    for j in range(random.randint(5,30)):
+        id_list.append(str(random.randint(0, 10000)))
+    st = str(caseid) + "\t" + str(label) + "\t" + ",".join(id_list)
+    print st
